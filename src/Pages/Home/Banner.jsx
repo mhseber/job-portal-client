2753860666cde2b8e46fe271949610d4e4b09fdf
mahamudul hasan seber <sebermh@gmail.com>
2024-12-11 03:53:55 +0600

    added animation

---
+++ src/Pages/Home/Banner.jsx
@@ -0,0 +1,40 @@
+import { motion } from "motion/react"
+import team1 from '../../assets/banner/pa1.jpg';
+import team2 from '../../assets/banner/pa2.jpg';
+const Banner = () => {
+    return (
+        <div className="hero bg-base-200 min-h-screen">
+            <div className="hero-content flex-col lg:flex-row-reverse">
+                <div className="flex-1">
+                    <motion.img
+                        src={team1}
+                        animate={{ y: [50, 100, 50] }}
+                        transition={{ duration: 10, repeat: Infinity }}
+                        className="max-w-sm w-64 rounded-t-[40px] rounded-br-[40px]  shadow-2xl" />
+                    <motion.img
+                        src={team2}
+                        animate={{ x: [100, 150, 100] }}
+                        transition={{ duration: 10, repeat: Infinity }}
+                        className="max-w-sm w-64 rounded-t-[40px] rounded-br-[40px]  shadow-2xl" />
+                </div>
+                <div className="flex-1">
+                    <motion.h1
+                        animate={{ x: 50 }}
+                        transition={{ duration: 0.3, delay: 1, ease: "linear" }}
+                        className="text-5xl font-bold">Latest <motion.span
+                            animate={{ color: ['#ecff33', '#33ffe3', '#ff6133'] }}
+                            transition={{ duration: 1.5, repeat: Infinity }}
+                        >job</motion.span> for you</motion.h1>
+
+                    <p className="py-6">
+                        Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem
+                        quasi. In deleniti eaque aut repudiandae et a id nisi.
+                    </p>
+                    <button className="btn btn-primary">Get Started</button>
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
