8f56244ddb944b11d53dda4e4dfd9778dfa03bd0
mahamudul hasan seber <sebermh@gmail.com>
2024-12-12 00:16:23 +0600

    mongoDB added

--- src/Pages/Home/Home.jsx
+++ src/Pages/Home/Home.jsx
@@ -1,9 +1,11 @@
+import HotJobs from "../HotJobs";
 import Banner from "./Banner";
 
 const Home = () => {
     return (
         <div className="">
             <Banner></Banner>
+            <HotJobs></HotJobs>
         </div>
     );
 };
---
+++ src/Pages/HotJobs.jsx
@@ -0,0 +1,23 @@
+import { useEffect, useState } from "react";
+import HotJobCard from "./HotJobCard";
+
+const HotJobs = () => {
+    const [jobs, setJobs] = useState([]);
+
+    useEffect(() => {
+        fetch('http://localhost:5000/job')
+            .then(res => res.json())
+            .then(data => setJobs(data))
+    }, []);
+    return (
+        <div>
+            <div>
+                {
+                    jobs.map(job => <HotJobCard key={job._id} job={job}></HotJobCard>)
+                }
+            </div>
+        </div>
+    );
+};
+
+export default HotJobs;
